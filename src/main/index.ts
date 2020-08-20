c69a86a8223b5b0fcacf2a237c24581709abf330
Bader Nasser <badersur@gmail.com>
2020-08-20 23:42:45 +0400

    Use node-fetch@3

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,27 +2,15 @@
 
 'use strict';
 
-import yargs from 'yargs';
-
 import {createApp} from './gui';
 import {getData} from './utils/get-data';
-import {version} from '../../package.json';
 import {getArguments} from './utils/get-cli-args';
 
 const argv = getArguments();
 
-if (argv.help || argv.version) {
-	if (argv.help) {
-		yargs.showHelp();
-	} else if (argv.version) {
-		console.log(version);
-	}
-
-	process.exit();
-} else if (argv.terminal || argv.cli) {
+if (argv.terminal || argv.cli) {
 	(async () => {
 		try {
-			console.log('Fetching data...');
 			const data = await getData();
 			console.dir(data.serverResponse);
 		} catch (error) {
--- src/main/utils/get-data.ts
+++ src/main/utils/get-data.ts
@@ -1,6 +1,11 @@
 import nodeFetch from 'node-fetch';
 
 export async function getData(): Promise<ServerData> {
+	const {node, electron} = process.versions;
+	console.log(
+		`Getting data using node.js v${node} & Electron v${electron}...`
+	);
+
 	const parameters = new URLSearchParams();
 	parameters.append('a', '1');
 
