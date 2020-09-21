db25e584a3f8237cb6fd7dd38e234e19fbb2b1ca
Bader Nasser <badersur@gmail.com>
2020-09-21 21:02:24 +0400

    Improve ServerResponse type

--- src/main/window.d.ts
+++ src/main/window.d.ts
@@ -1,4 +1,27 @@
-type ServerResponse = Record<string, unknown>;
+type ServerRecord = Record<string, unknown>;
+
+interface KnownServerResponse {
+	args: ServerRecord;
+	data: string;
+	files: ServerRecord;
+	form: ServerRecord;
+	headers:
+		| {
+				Accept: string;
+				'Accept-Encoding': string;
+				'Content-Length': string;
+				'Content-Type': string;
+				Host: string;
+				'User-Agent': string;
+				'X-Amzn-Trace-Id': string;
+		  }
+		| ServerRecord;
+	json: null | ServerRecord;
+	origin: string;
+	url: string;
+}
+
+type ServerResponse = KnownServerResponse | ServerRecord;
 
 interface ServerData {
 	serverResponse: ServerResponse;
